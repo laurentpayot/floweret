bddb529d2fb31dd935816cb4df25c993ac46328d
Laurent <laurent.payot@publiberty.com>
2018-10-03 13:44:16 +0200

    using transform-es2015-modules-simple-commonjs babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,5 @@
 module.exports = {
-	presets: [
-		[
-			"@babel/preset-env",
-			{
-				targets: {
-					esmodules: "umd"
-				}
-			}
-		]
-	],
+	//~ presets: [["@babel/preset-env", {targets: {esmodules: "umd"}}]],
+	plugins: ["transform-es2015-modules-simple-commonjs"],
 	comments: false
 }
