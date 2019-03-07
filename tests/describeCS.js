8c2fb597ccdc719443b4c9074a483db5a10c35a6
Laurent <laurent.payot@publiberty.com>
2019-03-07 19:51:21 +0100

    describeCS fix

--- tests/describeCS.js
+++ tests/describeCS.js
@@ -1,5 +1,8 @@
 // A "describe" callback must not return a value.
 // Returning a value from "describe" will fail the test in a future version of Jest.
 // => wrapping describe for use in CoffeeScript test files
+
 const describeJS = global.describe
-global.describe = (name, f) => describeJS(name, function () { f() })
+
+// NB: wrapping and assigning properties (.skip, .only etc.)
+global.describe = Object.assign((name, f) => describeJS(name, function () { f() }), describeJS)
